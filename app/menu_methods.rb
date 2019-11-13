f299c6bdab5827a8392506547091dde9fd3bc10e
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-13 11:07:18 -0500

    cleaned up a bit, added password options

--- app/menu_methods.rb
+++ app/menu_methods.rb
@@ -2,6 +2,22 @@ class Menu
 
   attr_reader :user, :username, :password
 
+#Basic runner for the main menu
+
+  def menu_runner
+
+    splash
+
+    login_menu_display
+    login_prompt
+
+
+
+
+  end
+
+#Helper Methods below!
+
   def splash
     puts " <<-SPLASH
       WELCOME TO THE TICKETMASTER SEARCHING EVENT CALENDAR APP
@@ -10,18 +26,43 @@ class Menu
       "
   end
 
-
-  def user_prompt 
+  def user_prompt
     puts ""
     user_input = gets.chomp
   end
 
-  def login_menu
+  def account_login
+    puts "Enter Username"
+    username_input = user_prompt
+    @user = User.find_or_create_by(username: username_input)
+
+    if @user.name
+      password_request
+    else
+
+  def login_menu_display
     puts "Welcome to Ticket Search app!"
     puts "What would you like to do?"
     puts "1. Create Account/Login"
     puts "2. Exit"
   end
+
+    def login_prompt
+      menu_choice = user_prompt
+
+      if menu_choice = 1
+        account_login
+
+      elsif menu_choice = 2
+        exit
+
+      else
+        puts "Sorry, incorrect option."
+        login_menu
+        
+      end
+    end
+
 
 
 
