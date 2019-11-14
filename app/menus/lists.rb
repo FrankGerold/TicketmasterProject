1999b54d142c5bd0ba3f0df661fcb1185a26aed0
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-14 15:56:18 -0500

    Searches work

--- app/menus/lists.rb
+++ app/menus/lists.rb
@@ -1,10 +1,7 @@
-
-
 def get_list (user_instance)
-
   list = user_instance.events
-
 end
+
 
 def friend_list
 
@@ -16,64 +13,67 @@ end
 
 def my_list
 
-  @user_list = get_list (@user)
+  user_list = get_list (@user)
 
   user_events_array(user_list)
 
 end
 
 
-def user_events_array(array)
-    counter = 0
-    exit_condition = counter + 1
-    selection_hash = {}
-    array.each do |event|
-        counter += 1
-        selection_hash[counter] = event
-        puts "#{counter}.)   #{event["name"]} #{event["date"]} #{event["start_time"]} #{event["event_type"]}"
-    end
-    puts "#{counter + 1}.) Go Back To Previous Menu."
-    puts "Which event would you like to choose?"
-    event_selection = user_prompt
-    if event_selection == exit_condition
-        event_search
-    elsif event_picked = selection_hash[event_selection.to_i]
-        user_detailed_view(event_picked)
-    else
-        puts "Invalid Selection. Please Try Again."
-        user_events_array(array)
-    end
+def user_events_array(events_array)
+    event_names = events_array.map { |event| event.name }
+    selected_event_name = @prompt.select('Pick an event.', event_names << "Go back.")
+    selected_event = events_array.find { |event| event.name  == selected_event_name }
+    selected_event_name == "Go back." ?  user_menu_runner : user_detailed_view(selected_event)
 end
 
 def user_detailed_view(event)
     puts "#{event["name"]} #{event["date"]} #{event["start_time"]} #{event["event_type"]}"
-
-    puts "What do you want to do?"
-    puts "1.) Delete Event"
-    puts "2.) Go Back"
-    detail_selection = user_prompt
-    user_detailed_menu(event, detail_selection)
-end
-
-def user_detailed_menu(event, selection)
-    if selection == "1"
+    option_select = @prompt.select("What do you want to do?", ["Delete Event", "Go Back"])
+    case option_select
+    when "Delete Event"
       user_event_list = @user.user_events
       dead_event = user_event_list.find do
-        |dead_event|
-        dead_event.id == event.id
-      end
+          |dead_event|
+          dead_event.id == event.id
+        end
+        dead_event.destroy
+        local_user = @user.id
+        @user = User.find(local_user)
+        @user_list = get_list (@user)
 
-      dead_event.destroy
-      local_user = @user.id
-      @user = User.find(local_user)
-      @user_list = get_list (@user)
-
-        user_events_array(@user_list)
-
-    elsif selection == "2"
-        user_events_array(@user_list)
-    else
-        puts "Incorrect Selection. Please try again"
-        user_detailed_view
+        puts "Event deleted."
+        user_menu_runner
+    when "Go Back"
+        user_events_array(Event.events)
     end
 end
+
+#
+# def user_detailed_view(event)
+#     puts "#{event["name"]} #{event["date"]} #{event["start_time"]} #{event["event_type"]}"
+#
+#     puts "What do you want to do?"
+#     puts "1.) Delete Event"
+#     puts "2.) Go Back"
+#     detail_selection = user_prompt
+#     user_detailed_menu(event, detail_selection)
+# end
+
+# def user_detailed_menu(event, selection)
+#     if selection == "1"
+#       user_event_list = @user.user_events
+#       dead_event = user_event_list.find do
+#         |dead_event|
+#         dead_event.id == event.id
+#       end
+#
+#       dead_event.destroy
+#       local_user = @user.id
+#       @user = User.find(local_user)
+#       @user_list = get_list (@user)
+#
+#         user_events_array(@user_list)
+#
+#     elsif selection == "2"
+#         user_events_array(@user_list)
